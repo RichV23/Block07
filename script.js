03d86faa6f9e610e52ef4fd872e52b5b11ab6153
Rich Villa <126531732+RichV23@users.noreply.github.com>
2023-03-30 01:07:08 -0700

    Block07 Repository Addition
    -Block07 Secure the Vault Workshop

---
+++ script.js
@@ -0,0 +1,16 @@
+//Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the Secret combination:
+let vaultString = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator. Each must equal one of the three codes in the combination.
+//Combination is 10-40-39
+//multiplication for 10
+let lock1 = 2*5;
+//Division for 40
+let lock2 = 120/3;
+//addition for 39
+let lock3 = 25+14;
+//Display the message and combination on the HTML page, or in an alert popup.,
+//Concat string to get disired popup
+console.log('${vaultString} ${lock1}-${lock2}-${lock3}');
+git remote add origin https://github.com/RichV23/Block07.git
+git branch -M main
+git push -u origin main
